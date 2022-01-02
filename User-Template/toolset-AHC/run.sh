03207e944abefb22a0ac175557009564ca1378ad
highjump425 <yuki.mizuno425@gmail.com>
2022-01-02 21:14:46 +0900

    [Delete] User-Template

--- User-Template/toolset-AHC/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-n='0000'$1
-echo 'result of seed' $1
-echo 'result of seed' $1 > ./result/temp/${n: -4:4}.txt
-./Source.exe ${@:2:($#-1)} < ./in/${n: -4:4}.txt > ./out/${n: -4:4}.txt 2>> ./result/temp/${n: -4:4}.txt
--- User-Template/toolset-MM/run.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-set local
-setlocal enabledelayedexpansion
-
-for /l %%a in (%1,1,%2) do (
-  echo result of seed %%a
-  java -jar tester.jar -exec ChickenRun.exe -pr -seed %%a
-)
